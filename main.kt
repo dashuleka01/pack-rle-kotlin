cd6117af3a6d0cfcd4f7ab0c3962044d5acefee2
dashuleka01 <dashka01012000@mail.ru>
2018-04-11 22:31:02 +0300

    z + u + test

--- main.kt
+++ main.kt
@@ -1,98 +1,116 @@
 import java.io.*
 import java.util.Scanner
 
-object main {
-    fun z(text: String, outputfile: String) {
-        val fr = FileReader(text)
-        val file = File(outputfile)
-        val fw = FileWriter(file)
-        val sc = Scanner(fr)
-        var result = ""
-        var j = 1
-        var single = 0
-        var signleSign = ""
-        while (sc.hasNextLine()) {
-            val line = sc.nextLine()
-            var i = line[0]
-            j = 1
-            for (k in line.substring(1, line.length)) {
-                if (k == i && j < 9)
-                    j++
-                else {
-                    if(j == 1){
-                        single++
-                        signleSign += i.toString()
-                        i = k
+
+fun z(text: String) {
+    val fr = FileReader(text)
+    val output = "output" + text
+    val file = File(output)
+    val fw = FileWriter(file)
+    val sc = Scanner(fr)
+
+    var result = ""
+    var j = 1
+    var single = 0
+    var signleSign = ""
+    var text = ""
+
+    while (sc.hasNextLine()) {
+        val line = sc.nextLine()
+        text += line
+        var i = line[0]
+        j = 1
+        for (k in line.substring(1, line.length)) {
+            if (k == i && j < 9)
+                j++
+            else {
+                if (j == 1) {
+                    single++
+                    signleSign += i.toString()
+                    i = k
+                } else {
+                    if (single > 0) {
+                        result += "-" + single.toString() + signleSign
+                        single = 0
+                        signleSign = ""
                     }
-                    else{
-                        if (single > 0) {
-                            result += "-" + single.toString() + signleSign
-                            single = 0
-                            signleSign = ""
+                    result += j.toString() + i
+                    j = 1
+                    i = k
+                }
+            }
+        }
+        if (j == 1) {
+            signleSign += i.toString()
+            single++
+        }
+        if (single > 0) {
+            result += "-" + single.toString() + signleSign
+            single = 0
+            signleSign = ""
+        }
+        if (j > 1)
+            result += j.toString() + i
+
+    }
+
+    if (result.length <= text.length)
+        fw.write(result)
+    else
+        fw.write(text)
+
+    fr.close()
+    fw.close()
+}
+
+fun u(text: String) {
+    val fr = FileReader(text)
+    val output = "output" + text
+    val file = File(output)
+    val fw = FileWriter(file)
+    val sc = Scanner(fr)
+    var result = ""
+    var flag = false
+    var count = 0
+
+    while (sc.hasNextLine()) {
+        for (i in sc.nextLine()) {
+            if (i == '-') {
+                flag = true
+            } else {
+                if (flag) { //одиночные символы
+                    if (count == 0)
+                        count = i.toString().toInt()
+                    else {
+                        result += i
+                        count--
+                    }
+                    if (count == 0)
+                        flag = false
+                } else {
+                    if (count == 0) { //кол-во символов
+                        count = i.toString().toInt()
+                    } else {
+                        for (j in 1..count) {
+                            result += i
                         }
-                        result += j.toString() + i
-                        j = 1
-                        i = k
+                        count = 0
                     }
                 }
             }
-            if(j == 1){
-                signleSign += i.toString()
-                single++
-            }
-
-            if (single > 0) {
-                result += "-" + single.toString() + signleSign
-                single = 0
-                signleSign = ""
-            }
-            if(j > 1)
-              result += j.toString() + i
-
         }
-        fw.write(result)
-
-        fr.close()
-        fw.close()
     }
+    fw.write(result)
 
-    fun u(text: String, outputfile: String) {
-        val fr = FileReader(text)
-        val file = File(outputfile)
-        val fw = FileWriter(file)
-        val sc = Scanner(fr)
-        var result = ""
-        var count = 0
-
-        while (sc.hasNextLine()) {
-            for (i in sc.nextLine()) {
-                if (i in '1'..'9')
-                    count += 1
-                else {
-                    for (j in 0..count - 1)
-                        result += i.toString()
-                    count = 0
-                }
-            }
-        }
-        fw.write(result)
-
-        fr.close()
-        fw.close()
-    }
+    fr.close()
+    fw.close()
+}
 
 
-    @Throws(Exception::class)
-    @JvmStatic
-    fun main(args: Array<String>) {
-        z(".\\input\\input3.txt", "output3.txt")
-        z(".\\input\\input1.txt", "output1.txt")
-        /*println("Введите команду, путь к исходному файлу и имя выходного файла через точку с запятой")
-        val list = (readLine() + "").split("; ")
-        when (list[0]) {
-            "z" -> z(list[1], list[2])
-            "u" -> u(list[1], list[2])
-            else -> println("Несуществующая команда")
-        }*/
+fun main(args: Array<String>) {
+    when (args[0]) {
+        "z" -> z(args[1])
+        "u" -> u(args[1])
+        else -> println("Несуществующая команда")
     }
 }
--- mainTest.kt
+++ mainTest.kt
@@ -1,20 +1,28 @@
 import org.junit.Assert.*
+import java.io.File
 
 class mainTest {
-    @org.junit.After
-    fun tearDown() {
-    }
-
-    @org.junit.Test
-    fun z() {
-    }
-
-    @org.junit.Test
-    fun u() {
+    private fun assertFileContent(name: String, expectedContent: String) {
+        val file = File(name)
+        val content = file.readLines().joinToString("\n")
+        assertEquals(expectedContent, content)
     }
 
     @org.junit.Test
     fun main() {
-    }
+        main(arrayOf("z", "input1.txt")) //тест, проверяющий сжатие "неудачных" данных
+        assertFileContent("outputinput1.txt",
+                "qwerty")
+        File("outputinput1.txt").delete()
 
+        main(arrayOf("u", "input2.txt")) //распаковка
+        assertFileContent("outputinput2.txt",
+                "bbaaakkkkkhjki")
+        File("outputinput2.txt").delete()
+
+        main(arrayOf("z", "input3.txt")) //упаковка
+        assertFileContent("outputinput3.txt",
+                "3a2b5k-6tnfjik")
+        File("outputinput3.txt").delete()
+    }
 }
